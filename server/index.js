f481e780fbb908ccc19fc3e5fd1c377f579c51d8
Syed Galib Basha <syedsunnygalibbasha@gmail.com>
2025-11-12 10:02:10 +0530

    Day 2: Added basic Express backend, MongoDB setup, and auth route working ✅

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("AI Assistance for Kerala Farmers - Backend running ✅");
 });
