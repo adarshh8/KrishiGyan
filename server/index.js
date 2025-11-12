84d41cdef396b8078af239a1d9813068453ead1d
Syed Galib Basha <syedsunnygalibbasha@gmail.com>
2025-11-12 09:23:54 +0530

    Day 2: Added user authentication (register & login)

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 const app = express();
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Atlas connected successfully"))
   .catch((err) => console.error("❌ MongoDB connection failed:", err));
-
-app.listen(process.env.PORT, () =>
-  console.log(`🚀 Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT 
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
